1d6ddaff118159b6627433e488fceb21d00a1f7e
João Paulo Pinheiro Teixeira <juaumlol@gmail.com>
2015-05-31 01:45:04 -0300

    test method calling view

--- src/CatenaLink-Response/ResponseCatenaLink.php
+++ src/CatenaLink-Response/ResponseCatenaLink.php
@@ -2,7 +2,19 @@
 namespace Amis\Catena\Link;
 
 use Amis\Catena\BaseCatenaLink;
-
+use Illuminate\Contracts\View\Factory as ViewFactory;
 class ResponseCatenaLink extends BaseCatenaLink {
     protected $name = "Response";
+    /**
+     * @var Illuminate\Contracts\View\Factory
+     */
+    private $_viewFactory;
+    public function __construct(ViewFactory $viewFactory){
+
+        $this->_response = $viewFactory;
+
+    }
+    public function renderView($viewName) {
+        return $this->_viewFactory->make($viewName);
+    }
 }
