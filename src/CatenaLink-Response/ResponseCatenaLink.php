d810ecb8033c1e41c5d64b25c8263cad17b3c2bb
João Paulo Pinheiro Teixeira <juaumlol@gmail.com>
2015-05-31 15:30:02 -0300

    comments

--- src/CatenaLink-Response/ResponseCatenaLink.php
+++ src/CatenaLink-Response/ResponseCatenaLink.php
@@ -3,17 +3,30 @@ namespace Amis\Catena\Link;
 
 use Amis\Catena\BaseCatenaLink;
 use Illuminate\Contracts\View\Factory as ViewFactory;
+
+/**
+ * This is a Link responsable for creating the Response for the Controller
+ * @package Amis\Catena\Link
+ */
 class ResponseCatenaLink extends BaseCatenaLink {
+    /** @var string $name */
     protected $name = "Response";
-    /**
-     * @var Illuminate\Contracts\View\Factory
-     */
+    /** @var Illuminate\Contracts\View\Factory $_viewFactory */
     private $_viewFactory;
+
+    /**
+     * Constructor with dependency injection
+     * @param ViewFactory $viewFactory
+     */
     public function __construct(ViewFactory $viewFactory){
-
         $this->_viewFactory = $viewFactory;
-
     }
+
+    /**
+     * Returns a view instance
+     * @param string $viewName
+     * @return \Illuminate\Contracts\View\View
+     */
     public function view($viewName) {
         return $this->_viewFactory->make($viewName);
     }
